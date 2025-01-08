f35e7f21a0953b362057a29ae59d8b9b714dd323
kanno <812137533@qq.com>
2025-01-08 16:23:57 +0800

    fix: zig build error

--- build.zig
+++ build.zig
@@ -130,6 +130,7 @@ fn build_node_bindings(
     });
     step_node_bindings.dependOn(&node_build_script.step);
     const copy_dist = b.addSystemCommand(&.{ "cp", "-r", "./bindings/node/dist", "./npm/zig-ini" });
+    copy_dist.step.dependOn(&node_build_script.step);
     // const copy_post_install = b.addSystemCommand(&.{ "cp", "-r", "./bindings/node/install.js", "./npm/zig-ini" });
     step_node_bindings.dependOn(&copy_dist.step);
     // step_node_bindings.dependOn(&copy_post_install.step);
